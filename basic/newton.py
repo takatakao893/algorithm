832a2f40c625adaa8bd5404f4165f2764e7953b5
takatakao893 <takafitber.qh802@gmail.com>
2023-02-09 18:06:57 +0900

    newton

---
+++ basic/newton.py
@@ -0,0 +1,18 @@
+def f(x):
+    return x**10.0-2
+
+def newton(x0,eps=1e-7,error=1e-7,max_loop=100,cnt=0):
+    while True:
+        df_x=(f(x0+eps)-f(x0-eps))/(2*eps)
+        if df_x <= error:
+            print("slope is too small")
+            exit()
+        
+        x1=x0-f(x0)/df_x
+        cnt+=1
+        if abs(x1-x0)<=error or cnt>=max_loop:
+            break
+        x0=x1
+    return x0
+
+print(newton(2.0))
